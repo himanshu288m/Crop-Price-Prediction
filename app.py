817a110290721d70672521f77949e96a42264bd8
himanshu288m <himanshugupta288@gmail.com>
2020-11-26 21:28:39 +0530

    Upload

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ def predict():
 			temp=list(json[0].values())
 			vals=np.array(temp)
 			prediction = lr.predict(temp)
-			print("here:",prediction)
-			        
+			print("here:",prediction)        
 			return jsonify({'prediction': str(prediction[0])})
 
 		except:        
@@ -29,13 +28,7 @@ def predict():
 	else:
 		return ('No model here to use')
     
-@app.route('/pred', methods=['GET'])
-def pred():
-	n = 0
-	lr = joblib.load("model.pkl")
-	if lr:
-		return predict()
-    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
