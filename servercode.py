01fb0eb3a85554d733465ed80d1b27467ea64a80
himanshu288m <himanshugupta288@gmail.com>
2020-11-26 14:57:19 +0530

    Upload

---
+++ servercode.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+import numpy as np
+import joblib
+import traceback
+from flask_restful import reqparse
+app = Flask(__name__)@app.route("/", methods=['GET'])
+def hello():
+    return "hey"@app.route('/predict', methods=['POST'])
+def predict():
+ lr = joblib.load("model.pkl")
+ if lr:
+  try:
+   json = request.get_json()  
+   model_columns = joblib.load("model_cols.pkl")
+   temp=list(json[0].values())
+   vals=np.array(temp)
+   prediction = lr.predict(temp)
+   print("here:",prediction)        
+   return jsonify({'prediction': str(prediction[0])})except:        
+   return jsonify({'trace': traceback.format_exc()})
+ else:
+  return ('No model here to use')if __name__ == '__main__':
+    app.run(debug=True)s
