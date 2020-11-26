99e0cf2922d13f7185ee5c2755232a3cf5432b29
himanshu288m <himanshugupta288@gmail.com>
2020-11-26 22:31:45 +0530

    Upload

--- CropPrice/lib/main.dart
+++ CropPrice/lib/main.dart
@@ -37,13 +37,18 @@ class Main extends StatelessWidget {
       if (resp.statusCode == 200) {
         print("DATA FETCHED SUCCESSFULLY");
         var result = json.decode(resp.body);
+        print(json.decode(resp.body));
         print(result["prediction"]);
+        if (result == null) {
+          print("Error");
+        }
         return result["prediction"];
       }
     } catch (e) {
       print("EXCEPTION OCCURRED: $e");
       return null;
     }
+    print("Error");
     return null;
   }
 
@@ -205,31 +210,72 @@ class Main extends StatelessWidget {
                 FlatButton(
                   color: Colors.blue,
                   onPressed: () async {
+                    // var body = [
+                    //   {
+                    //     "year": year,
+                    //     "month": month,
+                    //     "Com__Onion": comOnion,
+                    //     "Com__Potato": comPotato,
+                    //     "Com__Soyabean": comSoyabean,
+                    //     "Com__Tomato": comTomato,
+                    //     "Com__Wheat": comWheat,
+                    //     "(Red Nanital)": redNanital,
+                    //     "147 Average": average,
+                    //     "1st Sort": sort1,
+                    //     "Deshi": deshi,
+                    //     "Desi": desi,
+                    //     "Lok-1": lok1,
+                    //     "Lokwan": lokwan,
+                    //     "Onion": onion,
+                    //     "Other": other,
+                    //     "Sup": sup,
+                    //     "Yellow": yellow
+                    //   }
+                    // ];
+                    // var body = [
+                    //   {
+                    //     "year": 0.3,
+                    //     "month": 0.5,
+                    //     "Com__Onion": 0.8,
+                    //     "Com__Potato": 0.6,
+                    //     "Com__Soyabean": 0.3,
+                    //     "Com__Tomato": 0.6,
+                    //     "Com__Wheat": 0.7,
+                    //     "(Red Nanital)": 0.9,
+                    //     "147 Average": 1.0,
+                    //     "1st Sort": 1.0,
+                    //     "Deshi": 0.4,
+                    //     "Desi": 0.5,
+                    //     "Lok-1": 0.4,
+                    //     "Lokwan": 0.3,
+                    //     "Onion": 0.5,
+                    //     "Other": 0.3,
+                    //     "Sup": 0.3,
+                    //     "Yellow": 0.5
+                    //   }
+                    // ];
                     var body = [
                       {
-                        "year": year,
-                        "month": month,
-                        "Com__Onion": comOnion,
-                        "Com__Potato": comPotato,
-                        "Com__Soyabean": comSoyabean,
-                        "Com__Tomato": comTomato,
-                        "Com__Wheat": comWheat,
-                        "(Red Nanital)": redNanital,
-                        "147 Average": average,
-                        "1st Sort": sort1,
-                        "Deshi": deshi,
-                        "Desi": desi,
-                        "Lok-1": lok1,
-                        "Lokwan": lokwan,
-                        "Onion": onion,
-                        "Other": other,
-                        "Sup": sup,
-                        "Yellow": yellow
+                        "f0": 0.3,
+                        "f1": 0.5,
+                        "f2": 0.8,
+                        "f3": 0.6,
+                        "f4": 0.3,
+                        "f5": 0.6,
+                        "f6": 0.7,
+                        "f7": 0.9,
+                        "f8": 1.0,
+                        "f9": 1.0,
+                        "f10": 0.4,
+                        "f11": 0.5,
+                        "f12": 0.4,
+                        "f13": 0.3,
+                        "f14": 0.5,
+                        "f15": 0.3,
+                        "f16": 0.3,
+                        "f17": 0.5
                       }
                     ];
-                    /*body=[
-                    {"bedrooms": 3, "bathrooms": 1, "sqft_living": 1180, "sqft_lot": 5650, "floors": 1, "waterfront": 0, "view": 0, "condition": 3, "grade": 7, "sqft_above": 1180, "sqft_basement": 0, "lat": 47.5112, "long": -122.257, "sqft_living15": 1340, "sqft_lot15": 5650}
-                  ];*/
                     print(body);
                     var resp = await predictPrice(body);
                     _onBasicAlertPressed(context, resp);
