d99b8a6413ff06902d9ea44f367a0e8ff5d9aa29
himanshu288m <41584658+himanshu288m@users.noreply.github.com>
2020-12-05 07:36:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,9 +24,23 @@ def predict():
             modelfile = 'Xgmodel.pickle'
             model = p.load(open(modelfile,'rb'))
             print(model)
-            scaler = StandardScaler()
-            scaler.fit(data)
-            prediction = np.array2string(model.predict(data))
+            mean_ = np.array([2.01891029e+03, 6.38522427e+00, 1.97889182e-01, 1.82058047e-01,
+                            1.82058047e-01, 1.63588391e-01, 2.74406332e-01, 7.91556728e-03,
+                            8.97097625e-02, 2.90237467e-02, 7.38786280e-02, 8.44327177e-02,
+                            2.63852243e-03, 8.97097625e-02, 8.17941953e-02, 4.48548813e-01,
+                            2.63852243e-03, 8.97097625e-02])
+            var_ = np.sqrt(np.array([6.25197541e-01, 1.13397289e+01, 1.58729054e-01, 1.48912915e-01,
+                           1.48912915e-01, 1.36827229e-01, 1.99107497e-01, 7.85291108e-03,
+                           8.16619210e-02, 2.81813688e-02, 6.84205763e-02, 7.73038339e-02,
+                           2.63156063e-03, 8.16619210e-02, 7.51039049e-02, 2.47352775e-01,
+                           2.63156063e-03, 8.16619210e-02]))
+            mean_d = np.subtract(data,mean_)
+
+            norm_ = np.divide(mean_d,var_)
+            
+
+        
+            prediction = np.array2string(model.predict(norm_))
             return jsonify({'prediction': prediction})
 
         except:
